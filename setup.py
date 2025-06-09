33579f30fbb3b7d300cf39b8f1954f1f1a902b77
centervil <centervil.en@gmail.com>
2025-06-09 14:06:13 +0900

    Prepare release v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="content-converter",
-    version="0.1.0",
+    version="1.0.0",
     author="Centervil",
     author_email="info@centervil.example.com",
     description="マークダウンファイルを各種公開プラットフォーム用に変換するツール",
